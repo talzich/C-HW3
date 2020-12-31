8fcc750a8f0baeb0db86af9152ceb9ae564aa21f
Tal Zichlinsky <talzhtc@gmail.com>
2020-12-31 11:09:46 +0200

    does not work

--- txtfind.c
+++ txtfind.c
@@ -1,10 +1,22 @@
 #include <stdio.h> 
 
+#define LINE 256
+
+int get_line(char s[]){
+    char next;
+    int counter = 1;
+    for(int i = 0; s[i] != '\0'; i++){
+        scanf("%c",&next);
+        if(next == '\n'){
+            counter++;
+        }
+    }
+    return counter;
+}
+
 int main(void){
 
-    int a[] = {0,1,2,3,4};
-
-    int *ptr = a;
-    printf("%p\n", ptr);
-
+    char s[LINE];
+    printf("%d\n", get_line(s));
+    return 0;
 }
