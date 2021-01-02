6686edca900d82ba42f5783d450f629a765c0d05
Tal Zichlinsky <talzhtc@gmail.com>
2021-01-02 11:40:16 +0200

    Figured out which word and option we are searching for

--- txtfind.c
+++ txtfind.c
@@ -1,22 +1,56 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 
-#define LINE 256
+//This method takes input and dynamically places it into a string
+void get_string(char str[], int length){
+    int count = 0;
+    char c; 
 
-int get_line(char s[]){
-    char next;
-    int counter = 1;
-    for(int i = 0; s[i] != '\0'; i++){
-        scanf("%c",&next);
-        if(next == '\n'){
-            counter++;
-        }
+    while((c = getchar())!=EOF){
+        if (count >= length)
+            str = realloc(str, (length += 10) * sizeof(char));
+        str[count++] = c;
     }
-    return counter;
 }
 
-int main(void){
+void get_word(char str[], char word[], int len, char *option){
+    int count = 0;
+    char c;
 
-    char s[LINE];
-    printf("%d\n", get_line(s));
+    while(count < strlen(str)){
+        c = str[count];
+        if(count >= len)
+            word = realloc(word, (len += 5)*sizeof(char));
+        if(c != ' '){
+            word[count++] = c;
+        }
+        else break;
+    }
+    count++;
+    *option = str[count];
+}
+
+void print_lines(char str[], char word[]){}
+void print_words(char str[], char word[]){}
+
+
+int main(void){
+    int init_len = 10;
+    char option;//This char will hold the print option
+    char *str = malloc(init_len * sizeof(char));//This string will store the entire incoming string
+    char *word = malloc(init_len * sizeof(char));//This string will store the word we are looking for
+    
+    get_string(str, init_len);
+    get_word(str, word, init_len, &option);
+
+    // if(option == 'a') print_lines(str, word);
+    // else print_words(str, word);
+    
+    // printf("Word to search is %s, search option is %c\n", word, option);
+
+    char words[] = strtok(str, " ");
+    printf("%s\n", words);
+    
     return 0;
 }
